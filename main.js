6c5ce6b443fd110cd7437544edf5ac18d2e46847
youssef77569 <samyyoussef868@gmail.com>
2025-07-25 21:40:24 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+let img = document.querySelector('.img');
+let container = document.querySelector('.container');
+function phones(phone){
+    img.src = phone;
+
+}
+function colors(color){
+   container.style.background =  color;
+
+}
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+      }
+
+      form.classList.add('was-validated')
+    }, false)
+  })
+})()
